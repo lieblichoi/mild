e275850f6e01a698f29029fba8500f01b393a62e
Jaehwan Choi <lieblichoi@gmail.com>
2021-10-08 09:00:09 +0900

    Create SignUp

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,8 +1,31 @@
 import React from "react";
 import { Grid, Text, Input, Button } from "../elements";
 
+import {useDispatch} from "react-redux";
+import { actionCreators as userActions } from "../redux/modules/user";
+
 const SignUp = (props) => {
-    return (
+  const dispatch = useDispatch();
+
+  const [id, setId] = React.useState("");
+  const [pwd, setPwd] = React.useState("");
+  const [pwd_check, setPwdCheck] = React.useState("");
+  const [user_name, setUserName] = React.useState("");
+
+  const signup = () => {
+
+    if(id === "" || pwd === "" || user_name === "") {
+      return;
+    }
+
+    if(pwd !== pwd_check) {
+      return;
+    }
+
+    dispatch(userActions.signUpFB(id, pwd, user_name));
+  }
+
+  return (
       <React.Fragment>
         <Grid padding="16px">
           <Text size="32px" bold>
@@ -12,8 +35,8 @@ const SignUp = (props) => {
             <Input
               label="아이디"
               placeholder="아이디를 입력해주세요."
-              _onChange={() => {
-                console.log("!!");
+              _onChange={(e) => {
+                setId(e.target.value);
               }}
             ></Input>
           </Grid>
@@ -21,8 +44,8 @@ const SignUp = (props) => {
             <Input
               label="닉네임"
               placeholder="닉네임을 입력해주세요."
-              _onChange={() => {
-                console.log("!!");
+              _onChange={(e) => {
+                setUserName(e.target.value);
               }}
             ></Input>
           </Grid>
@@ -30,8 +53,8 @@ const SignUp = (props) => {
             <Input
               label="비밀번호"
               placeholder="비밀번호를 입력해주세요."
-              _onChange={() => {
-                console.log("!!");
+              _onChange={(e) => {
+                setPwd(e.target.value);
               }}
             ></Input>
           </Grid>
@@ -39,13 +62,13 @@ const SignUp = (props) => {
             <Input
               label="비밀번호 확인"
               placeholder="비밀번호를 한번 더 입력해주세요."
-              _onChange={() => {
-                console.log("!!");
+              _onChange={(e) => {
+                setPwdCheck(e.target.value);
               }}
             ></Input>
           </Grid>
 
-          <Button text="회원가입"></Button>
+          <Button text="회원가입" _onClick={signup}></Button>
         </Grid>
       </React.Fragment>
     );
--- src/shared/firebase.js
+++ src/shared/firebase.js
@@ -1,5 +1,5 @@
-import firebase from "firebase/app";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyB96e9gR9Wy4u0N7zEoxn5GJbeDYnZPHdY",
